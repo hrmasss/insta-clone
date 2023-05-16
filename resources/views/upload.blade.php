4889397e531b185c5bec08d589ed0f637149b8d8
hrmasss <hojayfarahman@gmail.com>
2023-05-17 00:37:54 +0600

    Deleted test resources

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -5,17 +5,38 @@
         <form class="rounded-xl flex justify-center items-center h-full flex-wrap w-3/4" action="/upload" method="POST"
             enctype="multipart/form-data">
             @csrf
-            <input type="file" id="fileInput" name="image" class="hidden">
-            <label for="fileInput"
-                class="cursor-pointer opacity-70 hover:opacity-100 transition-all duration-300 ease-in-out">
-                <img class="max-w-48 max-h-48" src="https://cdn-icons-png.flaticon.com/512/10025/10025551.png"
-                    alt="Upload">
+            <input type="file" id="file-input" name="image" class="hidden" onchange="showPreview(event);">
+            <label for="file-input" class="cursor-pointer">
+                <img id="preview" class="rounded-xl max-h-80 hover:max-h-96 transition-all duration-300 ease-in-out"
+                    src="https://cdn-icons-png.flaticon.com/512/10025/10025551.png" alt="Upload">
             </label>
-            <h1 class="my-5 text-3xl font-[Rochester] tracking-wide text-secondary w-full text-center">Share your moments
+            <h1 class="my-5 text-3xl font-[Rochester] tracking-wide text-secondary w-full text-center hide-after-select">
+                Share your moments
             </h1>
-            <textarea class="w-full my-5 p-5 bg-transparent resize-none outline-none" name="caption" rows="1"
-                placeholder="WRITE A CAPTION"></textarea>
-            <button type="submit" class="btn btn-block">Post</button>
+            <textarea hidden class="w-full my-5 p-5 bg-transparent resize-none outline-none reveal-after-select" name="caption"
+                rows="1" placeholder="WRITE A CAPTION"></textarea>
+            <button hidden type="submit" class="btn btn-block reveal-after-select">Post</button>
         </form>
     </div>
+
+    <script>
+        const input = document.getElementById('file-input');
+        const preview = document.getElementById('preview');
+        const itemsToHide = document.getElementsByClassName('hide-after-select');
+        const itemsToReveal = document.getElementsByClassName('reveal-after-select');
+
+        function showPreview(event) {
+            if (input.files[0]) {
+                preview.src = URL.createObjectURL(input.files[0]);
+            }
+
+            [...itemsToHide].forEach(item => {
+                item.setAttribute("hidden", true);
+            });
+
+            [...itemsToReveal].forEach(item => {
+                item.removeAttribute("hidden");;
+            });
+        }
+    </script>
 @endsection
